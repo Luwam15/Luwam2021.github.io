322daf369a1ca8e6322e1e8b00f311d84c22ac8c
Luwam-P <78628437+Luwam-P@users.noreply.github.com>
2021-09-17 12:07:51 -0500

    Update destructure.js

--- cs305/d25destructure/destructure.js
+++ cs305/d25destructure/destructure.js
@@ -1,8 +1,12 @@
 "use strict";
-/* eslint-disable */
-/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-module.exports = { topSalary, getDateAgo }; //add all of your function names here that you need for the node mocha tests
 
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+//module.exports = { topSalary, getDateAgo }; //add all of your function names here that you need for the node mocha tests
+/**
+ *
+ * @param {object} salaries array of people
+ * @returns{object} person with highest salary
+ */
 function topSalary(salaries) {
   let high = -Infinity;
   let rich = null;
