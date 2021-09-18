27b1668ff61624c1238a06522332d5c482264e78
Luwam-P <78628437+Luwam-P@users.noreply.github.com>
2021-09-17 20:43:50 -0500

    Update bank.js

--- cs305/d34scheduledCallbacks/bank.js
+++ cs305/d34scheduledCallbacks/bank.js
@@ -2,7 +2,7 @@
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser.
  */
-//module.exports = { makeBank }; //add all of your function names here that you need for the node mocha tests
+module.exports = { makeBank }; //add all of your function names here that you need for the node mocha tests
 
 /* 2.	In the following code the transactionsDB is publicly accessible to any code that has access to the bank object.  
 Instead of using the object literal for bank, write a makeBank function that will encapsulate and return the bank object. 
@@ -13,17 +13,16 @@ Make the transactionsDB private by making it a local variable in the makeBank fu
  * @returns {bank} factory function that builds and returns bank object
  */
 function makeBank() {
-  //implement this
-  const bank = { transactionsDB: [] };
-
-  bank.transactionsDB = [
+  let transactionsDB = [
     { customerId: 1, customerTransactions: [10, 50, -40] },
     { customerId: 2, customerTransactions: [10, 10, -10] },
     { customerId: 3, customerTransactions: [5, -5, 55] },
   ];
 
+  const bank = {};
+
   bank.getBalance = function (cId) {
-    const customer = bank.transactionsDB.find(
+    const customer = transactionsDB.find(
       (customer) => customer.customerId === cId
     );
     let balance = 0;
@@ -35,7 +34,7 @@ function makeBank() {
 
   bank.bankBalance = function () {
     let total = 0;
-    for (const trans of this.transactionsDB) {
+    for (const trans of transactionsDB) {
       total += this.getBalance(trans.customerId);
     }
     return total;
