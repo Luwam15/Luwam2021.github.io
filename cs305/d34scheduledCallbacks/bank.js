68d5f697291904b7b0cc2375cfedbea328cc09fe
Luwam-P <78628437+Luwam-P@users.noreply.github.com>
2021-09-17 22:25:46 -0500

    Update bank.js

--- cs305/d34scheduledCallbacks/bank.js
+++ cs305/d34scheduledCallbacks/bank.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { setTimeout } = require("timers/promises");
+
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser.
  */
 module.exports = { makeBank }; //add all of your function names here that you need for the node mocha tests
